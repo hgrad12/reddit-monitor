34722789f8156f75066fe5aad1b4ea1b69e90ec0
Samuel Oden <samuel.aoden@gmail.com>
2025-04-10 22:48:58 -0600

    feat: adding Result equals

--- internal/entity.go
+++ internal/entity.go
@@ -11,6 +11,29 @@ type Result struct {
 	TopPosters []TopUsers
 }
 
+func (r *Result) Equals(o *Result) bool {
+	if r.Name != o.Name {
+		return false
+	}
+	if len(r.HotPosts) != len(o.HotPosts){
+        return false
+    }
+	for i, hobby := range r.HotPosts {
+        if hobby != o.HotPosts[i]{
+            return false
+        }
+    }
+	if len(r.TopPosters) != len(o.TopPosters){
+        return false
+    }
+    for i, hobby := range r.TopPosters {
+        if hobby != o.TopPosters[i]{
+            return false
+        }
+    }
+	return true
+}
+
 type TopUsers struct {
 	Author   string
 	NumberOfPosts int
